b60fe46a3495a8baeaf894cd129c2f901babda71
Daniel Myers <daniel.myers@snowflake.com>
2023-06-26 09:20:41 -0700

    Update Test_App_Locally.sql

--- app/test/Test_App_Locally.sql
+++ app/test/Test_App_Locally.sql
@@ -2,13 +2,15 @@
 -- DB SETUP
 -- #########################################
 
-CREATE OR REPLACE DATABASE NATIVE_APP_SUMMIT_DB;
-USE DATABASE NATIVE_APP_SUMMIT_DB;
+CREATE OR REPLACE DATABASE NATIVE_APP_QUICKSTART_DB;
+USE DATABASE NATIVE_APP_QUICKSTART_DB;
 
-CREATE OR REPLACE SCHEMA NATIVE_APP_SUMMIT_SCHEMA;
-USE SCHEMA NATIVE_APP_SUMMIT_SCHEMA;
+CREATE OR REPLACE SCHEMA NATIVE_APP_QUICKSTART_SCHEMA;
+USE SCHEMA NATIVE_APP_QUICKSTART_SCHEMA;
 
-CREATE OR REPLACE STAGE NATIVE_APP_SUMMIT_STAGE;
+CREATE OR REPLACE STAGE NATIVE_APP_QUICKSTART_STAGE;
+
+CREATE OR REPLACE WAREHOUSE NATIVE_APP_QUICKSTART_WH WAREHOUSE_SIZE=SMALL INITIALLY_SUSPENDED=TRUE;
 
 CREATE OR REPLACE TABLE MFG_ORDERS (
   order_id NUMBER(38,0), 
@@ -42,33 +44,28 @@ CREATE OR REPLACE TABLE MFG_SITE_RECOVERY (
 -- Load app/data/site_recovery_data.csv using Snowsight
 
 ################################################################
-Create application package
-################################################################
-
-
-################################################################
 Create SHARED_CONTENT_SCHEMA to share in the application package
 ################################################################
 use database HELLO_SNOWFLAKE_PACKAGE;
 create schema shared_content_schema;
 
 use schema shared_content_schema;
-create or replace view MFG_SHIPPING as select * from NATIVE_APP_SUMMIT_DB.NATIVE_APP_SUMMIT_SCHEMA.MFG_SHIPPING;
+create or replace view MFG_SHIPPING as select * from NATIVE_APP_QUICKSTART_DB.NATIVE_APP_QUICKSTART_SCHEMA.MFG_SHIPPING;
 
 grant usage on schema shared_content_schema to share in application package HELLO_SNOWFLAKE_PACKAGE;
-grant reference_usage on database NATIVE_APP_SUMMIT_DB to share in application package HELLO_SNOWFLAKE_PACKAGE;
+grant reference_usage on database NATIVE_APP_QUICKSTART_DB to share in application package HELLO_SNOWFLAKE_PACKAGE;
 grant select on view MFG_SHIPPING to share in application package HELLO_SNOWFLAKE_PACKAGE;
 
 -- ################################################################
 -- TEST APP LOCALLY
 -- ################################################################
 
-USE DATABASE NATIVE_APP_SUMMIT_DB;
-USE SCHEMA NATIVE_APP_SUMMIT_SCHEMA;
+USE DATABASE NATIVE_APP_QUICKSTART_DB;
+USE SCHEMA NATIVE_APP_QUICKSTART_SCHEMA;
 
 -- This executes "setup.sql" linked in the manifest.yml; This is also what gets executed when installing the app
-CREATE APPLICATION NATIVE_APP_SUMMIT_APP FROM application package HELLO_SNOWFLAKE_PACKAGE using version 1 patch 0;
+CREATE APPLICATION NATIVE_APP_QUICKSTART_APP FROM application package HELLO_SNOWFLAKE_PACKAGE using version 1 patch 0;
 -- For example, CREATE APPLICATION LEAD_TIME_OPTIMIZER_APP FROM application package LEAD_TIME_OPTIMIZER_PKG using version V1 patch 0;
 
--- At this point you should see and run the app NATIVE_APP_SUMMIT_APP listed under Apps
+-- At this point you should see and run the app NATIVE_APP_QUICKSTART_APP listed under Apps
 SHOW APPLICATION PACKAGES;
